1209f0006f69e119c069dbc2fa1bcd365ef16dc1
Kang Wang <kwang.inbox@gmail.com>
2017-12-16 15:40:31 -0800

    Altered /register POST

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors);
 app.use(morgan('combine')); // see docs
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => {
-  res.end('Hello World');
+app.post('/register', (req, res) => {
+  res.send({
+    message: 'Hello!. You\'ve successfully registered!',
+  });
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('we are up at on port ', process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081, () => {
+  console.log('we are up at on port ', process.env.PORT || 8081);
 });

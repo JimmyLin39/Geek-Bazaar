a77682c34d01f0cc2d8d653536621f63ff5f4f7d
Kang Wang <kwang.inbox@gmail.com>
2017-12-15 19:00:57 -0800

    Bare bones express server up.

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('combine')); // see docs
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.end('Hello World');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('we are up at on port ', process.env.PORT || 8080);
+});

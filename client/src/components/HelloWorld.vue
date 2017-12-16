b03cc0f8c3f40414eb68655999ecee87ccbee968
JimmyLin39 <jimmylin0309@email.com>
2017-12-15 17:26:59 -0800

    add bootstrap view testing

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -26,8 +26,8 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 export default {
   name: 'HelloWorld',

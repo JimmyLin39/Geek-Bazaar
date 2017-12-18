32d60db0d6ce74aec5e495bbfd3b1647755c4018
JimmyLin39 <jimmylin0309@email.com>
2017-12-18 15:02:00 -0800

    add user info

--- server/src/db/seeds/a_users.js
+++ server/src/db/seeds/a_users.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        { full_name: 'Alice', display_name: 'Alice', email: 'alice@user.com', password: 'alice', address: 'main street' },
+        { full_name: 'Bob', display_name: 'Bob', email: 'bob@user.com', password: 'bob', address: 'main street' },
+        { full_name: 'Charlie', display_name: 'Charlie', email: 'charlie@user.com', password: 'charlie', address: 'main street' }
       ]);
     });
 };

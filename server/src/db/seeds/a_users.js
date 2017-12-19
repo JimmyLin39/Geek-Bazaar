804ef299b84c595068e5dc03752e34d122dd938c
JimmyLin39 <jimmylin0309@email.com>
2017-12-18 16:06:09 -0800

    add id and update id according to table length

--- server/src/db/seeds/a_users.js
+++ server/src/db/seeds/a_users.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
+  const users = [
+    { id: 1, full_name: 'Alice', display_name: 'Alice', email: 'alice@user.com', password: 'alice', address: 'main street' },
+    { id: 2, full_name: 'Bob', display_name: 'Bob', email: 'bob@user.com', password: 'bob', address: 'main street' },
+    { id: 3, full_name: 'Charlie', display_name: 'Charlie', email: 'charlie@user.com', password: 'charlie', address: 'main street' }
+  ];
+
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        { full_name: 'Alice', display_name: 'Alice', email: 'alice@user.com', password: 'alice', address: 'main street' },
-        { full_name: 'Bob', display_name: 'Bob', email: 'bob@user.com', password: 'bob', address: 'main street' },
-        { full_name: 'Charlie', display_name: 'Charlie', email: 'charlie@user.com', password: 'charlie', address: 'main street' }
-      ]);
+      return knex('users')
+        .insert(users)
+        .then(() => 
+          knex.raw(`ALTER SEQUENCE users_id_seq RESTART WITH ${users.length + 1}`)
+        );  
     });
 };
--- server/src/db/seeds/c_inventories.js
+++ server/src/db/seeds/c_inventories.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
+  const inventories = [
+    {id: 1, name: '10 Days in Africa', description: 'You have 10 DAYS IN AFRICA', price: '80', condition: 'new', product_id: 1, user_id: 1},
+    {id: 2, name: '10 Days in Africa', description: 'You have 10 DAYS IN AFRICA', price: '20', condition: 'good', product_id: 1, user_id: 2},
+    {id: 3, name: '10 Days in Europe', description: 'There is much to be discovered in Europe!', price: '20', condition: 'like new', product_id: 2, user_id: 3},
+  ];
+
   // Deletes ALL existing entries
   return knex('inventories').del()
     .then(function () {
       // Inserts seed entries
-      return knex('inventories').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('inventories')
+        .insert(inventories)
+        .then(() => 
+          knex.raw(`ALTER SEQUENCE inventories_id_seq RESTART WITH ${inventories.length + 1}`)
+        );
     });
 };

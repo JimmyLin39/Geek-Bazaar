1711695e79b6bef80695644ea06e365763dc12f1
Kang Wang <kwang.inbox@gmail.com>
2017-12-19 13:00:40 -0800

    Added <head> section from Jumbotron example to template section

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../favicon.ico">
+
+      <title>Geek Bazaar</title>
+
+      <!-- Bootstrap core CSS -->
+      <link href="../bootstrap-4.0.0-alpha.6/dist/css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Custom styles for this template -->
+      <link href="../bootstrap-4.0.0-alpha.6/docs/examples/jumbotron/jumbotron.css" rel="stylesheet">
+    </head>
     <header class=“app-header”>
       <Navbar></Navbar>
+      <!-- <Titlebar></Titlebar> -->
+      <!-- <Pagefooter></Pagefooter> -->
     </header>
     <main class=“container”>
       <!-- <router-view></router-view> -->
@@ -11,10 +28,14 @@
 
 <script>
 import Navbar from './components/Navbar';
+// import Titlebar from './components/Titlebar';
+// import Pagefooter from './components/Pagefooter';
 
 export default {
   components: {
     Navbar,
+    // Titlebar,
+    // Pagefooter
   },
 
 //  created () {
@@ -24,4 +45,5 @@ export default {
 </script>
 
 <style>
+
 </style>

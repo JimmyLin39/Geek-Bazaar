b1c47674f45126527bc7a5ae4315ac9de46de864
JimmyLin39 <jimmylin0309@email.com>
2017-12-19 18:00:35 -0800

    implement search function

---
+++ client/src/components/Autocomplete.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+  <b-nav-form>
+  <b-form-input 
+    size="sm" 
+    class="mr-sm-2" 
+    type="text" 
+    placeholder="Search"
+    v-model='searchInput'
+  />
+  <b-button size="sm" class="my-2 my-sm-0" type="submit" @click='search'>Search</b-button>
+  </b-nav-form>
+  <li v-for="n in searchResult">{{ n }}</li>
+  </div>
+</template>
+
+<script>
+import BggApi from '@/services/BggApi'
+export default {
+  data() {
+    return {
+      searchInput: '',
+      searchResult: [],
+    };
+  },
+  methods: {
+    async search() {
+      const response = await BggApi.searchResult(
+        this.searchInput
+      )
+      const responseData = response.data.allResults;
+      console.log(responseData);
+      responseData.forEach( (element) => {
+        const boardgameItem = element.items.item
+        if (boardgameItem.name.length > 0) {          
+          console.log(boardgameItem.name[0].value, boardgameItem.yearpublished.value);
+          this.searchResult.push(boardgameItem.name[0].value);
+        } else {
+          console.log(boardgameItem.name.value); 
+          this.searchResult.push(boardgameItem.name.value);       
+        }
+      });
+    }
+  }
+}
+</script>

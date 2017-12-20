ea0036497276c6c3edf87f5e5a503bcf2759920e
JimmyLin39 <jimmylin0309@email.com>
2017-12-20 14:35:47 -0800

    add /inventories routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config({path: '../.env'});
 
+const PORT = process.env.PORT || 8081;
 const ENV = process.env.ENV || 'development';
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +12,9 @@ const cors = require('cors');
 
 const app = express();
 
+// Seperated Routes for each Resource
+const inventoriesRoutes = require('./routes/inventories');
+
 app.use(cors());
 app.use(morgan('dev')); // see docs
 
@@ -18,10 +22,15 @@ app.use(morgan('dev')); // see docs
 app.use(knexLogger(knex));
 app.use(bodyParser.json());
 
+// Mount all resource routes
+app.use('/inventories', inventoriesRoutes(knex));
+
 app.post('/register', (req, res) => {
   res.send({
     message: `Hello ${req.body.email}!  You've successfully registered!`,
   });
 });
 
-app.listen(process.env.PORT || 8081);
+app.listen(PORT, () => {
+  console.log('Geek-Bazzar Api listening on port ' + PORT);
+});

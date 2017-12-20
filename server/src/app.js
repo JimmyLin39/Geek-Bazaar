eff9f11a34b2d55f4a3f9d15194d5c60be7f4417
Kang Wang <kwang.inbox@gmail.com>
2017-12-20 14:25:29 -0800

    knex logic to insert new user

--- server/src/app.js
+++ server/src/app.js
@@ -14,14 +14,23 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev')); // see docs
 
-// Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 app.use(bodyParser.json());
 
+// TODO: Create new user instances with Knex:
 app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}!  You've successfully registered!`,
-  });
+  const newUser = {
+    full_name: req.body.full_name,
+    display_name: req.body.display_name,
+    email: req.body.email,
+    password: req.body.password,
+  }
+  knex.insert(newUser)
+    .into('users')
+    .then(res.send({
+        message: `Hello ${req.body.full_name}!  You've successfully registered with the display name ${req.body.display_name}, and email ${req.body.email}`,
+      })
+    );
 });
 
 app.listen(process.env.PORT || 8081);

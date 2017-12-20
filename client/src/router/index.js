7b33db5bca4b2e42e107ad3050ee854e4b1ef636
Kang Wang <kwang.inbox@gmail.com>
2017-12-19 17:04:37 -0800

    Route to login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Register from '@/components/Register';
 import LandingPage from '@/components/LandingPage';
+import Login from '@/components/Login'
 
 Vue.use(Router);
 
@@ -19,5 +20,10 @@ export default new Router({
       name: 'Register',
       component: Register,
     },
+    {
+      path: '/login',
+      name: 'Login', 
+      compponent: Login,
+    }
   ],
 });

9cf25b6054d46cedb0d18694d9ad0f38ef79a2a3
JimmyLin39 <jimmylin0309@email.com>
2017-12-20 13:44:44 -0800

    update global styles

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// import global styles
+import './styles/style.scss';
 
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';

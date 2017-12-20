b386a376fcc44854903bd527c17501ec0b3b38e5
JimmyLin39 <jimmylin0309@email.com>
2017-12-20 12:51:29 -0800

    add card component

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>Hello User!</h1>
+<div class="container">
+  <section class="row">
+    <div class="col-sm-6 col-md-4">
+      <section class="card card-product">
+        <figure class="card-img-top card-product-image">
+          <img src="../assets/logo.png" alt="Product image">
+        </figure>
+        <div class="card-content">
+          <aside class="card-overlay">
+            <button class="btn btn-outline-primary add-to-cart" type="button" name="button">Add to cart</button>
+          </aside>
+          <div class="card-body">
+            <header class="product-info">
+              <h1 class="title">product.name</h1>
+              <span class="price">product.price</span>
+            </header>
+            <p class="description">product.description</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  </section>
+</div>
 </template>
+
+<style lang="scss" scoped>
+.add-to-cart {
+  margin: 0 auto;
+}
+
+.product-info {
+  display: flex;
+  align-items: center;
+
+
+  .title {
+    flex-grow: 1;
+    margin: 0;
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 500;
+  }
+
+  .price {
+    margin-left: .5rem;
+    font-size: 1.25rem;
+    font-weight: 700;
+    flex-shrink: 0;
+  }
+}
+
+.description {
+  margin: 16px 0 0;
+  display: none;
+}
+
+.card-product:hover .description {
+  display: block;
+}
+
+</style>

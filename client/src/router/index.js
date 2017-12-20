722751fcde86aaa8cecf777bef19e41a9244b17f
Kang Wang <kwang.inbox@gmail.com>
2017-12-19 19:01:12 -0800

    Corrected stupid spelling mistake!

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Register from '@/components/Register';
 import LandingPage from '@/components/LandingPage';
-import Login from '@/components/Login'
+import Login from '@/components/Login';
 
 Vue.use(Router);
 
@@ -23,7 +23,7 @@ export default new Router({
     {
       path: '/login',
       name: 'Login', 
-      compponent: Login,
+      component: Login,
     }
   ],
 });

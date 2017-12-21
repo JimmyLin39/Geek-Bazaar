aeae55f06582dd3d46a6399ff14ac4fda043ba44
Kang Wang <kwang.inbox@gmail.com>
2017-12-20 16:39:20 -0800

    Removed commented out sections

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,30 +1,29 @@
 <template>
   <div class='login-container'>
-    <div class="container">    
-      <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+    <div class="container">
+      <div id="loginbox" style="margin-top:50px;"
+        class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info" >
           <div class="panel-heading">
             <div class="panel-title">Log In</div>
-            <!-- <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div> -->
-          </div>     
+          </div>
           <div style="padding-top:30px" class="panel-body"></div>
-            <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>              
-              <form id="loginform" class="form-horizontal" role="form">                    
+            <div style="display:none" id="login-alert"
+              class="alert alert-danger col-sm-12"></div>
+              <form id="loginform" class="form-horizontal" role="form">
                 <div style="margin-bottom: 25px" class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                  <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Email">                                        
-                </div>              
+                  <span class="input-group-addon"><i
+                    class="glyphicon glyphicon-user"></i></span>
+                  <input id="login-email" type="text" class="form-control"
+                    name="email" value="" placeholder="Email"
+                    v-model='email'>
+                </div>
                 <div style="margin-bottom: 25px" class="input-group">
                   <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                  <input id="login-password" type="password" class="form-control" name="password" placeholder="Password">
-                </div>                  
-                <!-- <div class="input-group">
-                  <div class="checkbox">
-                    <label>
-                      <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-                    </label>
-                  </div>
-                </div> -->
+                  <input id="login-password" type="password" class="form-control"
+                    name="password" placeholder="Password"
+                    v-model='password'>
+                </div>
                 <div style="margin-top:10px" class="form-group">
                   <div class="col-sm-12 controls">
                     <a id="btn-login" href="#" class="btn btn-success">Login  </a>
@@ -33,14 +32,14 @@
                 <div class="form-group">
                   <div class="col-md-12 control">
                     <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                      Don't have an account! 
+                      Don't have an account!
                       <a href="#/register">Register Here</a>
                     </div>
                   </div>
-                </div>    
-              </form>     
-            </div>                     
-          </div>  
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
 
         <div id="signupbox" style="display:none; margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
@@ -49,20 +48,20 @@
             <div class="panel-heading">
               <div class="panel-title">Sign Up</div>
               <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
-            </div>  
+            </div>
 
             <div class="panel-body">
-              <form id="signupform" class="form-horizontal" role="form">                    
+              <form id="signupform" class="form-horizontal" role="form">
                 <div id="signupalert" style="display:none" class="alert alert-danger">
                   <p>Error:</p>
                   <span></span>
-                </div>                          
+                </div>
                 <div class="form-group">
                   <label for="email" class="col-md-3 control-label">Email</label>
                   <div class="col-md-9">
                     <input type="text" class="form-control" name="email" placeholder="Email Address">
                   </div>
-                </div>  
+                </div>
                 <div class="form-group">
                   <label for="firstname" class="col-md-3 control-label">First Name</label>
                   <div class="col-md-9">
@@ -87,23 +86,23 @@
                       <input type="text" class="form-control" name="icode" placeholder="">
                   </div>
                 </div>
-                <div class="form-group">                              
+                <div class="form-group">
                   <div class="col-md-offset-3 col-md-9">
                     <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Sign Up</button>
-                    <span style="margin-left:8px;">or</span>  
+                    <span style="margin-left:8px;">or</span>
                   </div>
                 </div>
-                <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group"> 
+                <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group">
                   <div class="col-md-offset-3 col-md-9">
                       <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   Sign Up with Facebook</button>
-                  </div>                                             
+                  </div>
                 </div>
               </form>
             </div>
 
           </div>
 
-        </div> 
+        </div>
 
       </div>
     </div>
@@ -117,29 +116,25 @@ export default {
   data() {
     return {
       email: '',
-      password: '', 
-      firstName: '',
-      lastName: ''
+      password: '',
     };
   },
   methods: {
-    // TODO: point the registration response to reference the '/register'
-    // end point in the Express backend -->
     async login() {
       const response = await AuthenticationService.login({
         email: this.email,
         password: this.password
       })
       console.log(response.data)
-    }
-  } 
+    },
+  }
 };
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.login-container, 
+.login-container,
 .mainbox,
 .form-horizontal,
 .panel .panel-info,

9fa247dfe57f8710c133797424e2f098e2a6c1f5
Kang Wang <kwang.inbox@gmail.com>
2017-12-21 15:30:01 -0800

    User emails must be unique upon registration

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-// console.log(process.env.SESSION_SECRET);
 
 const PORT = process.env.PORT || 8081;
 const ENV = process.env.ENV || 'development';
@@ -17,6 +16,7 @@ const app = express();
 
 // Seperated Routes for each Resource
 const inventoriesRoutes = require('./routes/inventories');
+const usersRoutes = require('./routes/users');
 
 app.use(cors());
 app.use(morgan('dev')); // see docs
@@ -26,21 +26,42 @@ app.use(bodyParser.json());
 
 // Mount all resource routes
 app.use('/inventories', inventoriesRoutes(knex));
+// app.use('/users');
 
 app.post('/register', (req, res) => {
-  const newUser = {
-    full_name: req.body.full_name,
-    display_name: req.body.display_name,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 10),
-  }
-  knex.insert(newUser)
-    .into('users')
-    .then(res.send({
-        message: `Hello ${req.body.full_name}!  You've successfully registered with the display name ${req.body.display_name}, and email ${req.body.email}`,
-      })
-    );
-});
+
+  // if (!req.body.full_name || !req.body.display_name || ! req.body.email ||
+  //   req.body.password) {
+  //   res.json({
+  //     message: 'All the fields must be filled!'
+  //   })
+  // } else {
+  knex('users')
+    .where({email: req.body.email})
+    .then((results) => {
+      if (results.length) {
+        // console.log(results)
+        return res.json({
+          message: 'Email is already registered!'
+        })
+      } else {
+        const newUser = {
+          full_name: req.body.full_name,
+          display_name: req.body.display_name,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10),
+        }
+        knex.insert(newUser)
+          .into('users')
+          .then(res.json({
+            message: `Hello ${req.body.full_name}!
+            You've successfully registered with the display name
+            ${req.body.display_name}, and email ${req.body.email}.`,
+          }))
+      }
+    })
+  })
+// require('./routes')(app)
 
 app.listen(PORT, () => {
   console.log('Geek-Bazzar Api listening on port ' + PORT);

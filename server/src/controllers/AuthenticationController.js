cd22fc110aca395e8116d9f86d0a84264359f013
Kang Wang <kwang.inbox@gmail.com>
2017-12-21 10:53:38 -0800

    LF bs and package update

---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,24 @@
+const ENV = process.env.ENV || 'development';
+const bcrypt = require('bcrypt');
+const knexConfig = require('../knexfile');
+const knex = require('knex')(knexConfig[ENV]);
+const knexLogger = require('knex-logger');
+
+module.exports = {
+  register (req, res) {
+    const newUser = {
+      full_name: req.body.full_name,
+      display_name: req.body.display_name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+    }
+    knex.insert(newUser)
+      .into('users')
+      .then(res.send({
+          message: `Hello ${req.body.full_name}!
+          You've successfully registered with the display name
+          ${req.body.display_name}, and email ${req.body.email}`,
+        })
+      );
+  }
+}

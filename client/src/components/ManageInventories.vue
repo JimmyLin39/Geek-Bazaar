e87e4fc27d2feb9a5caefab3dbe9e84df5b3d353
JimmyLin39 <jimmylin0309@email.com>
2017-12-21 17:58:28 -0800

    add dix userId and product id

--- client/src/components/ManageInventories.vue
+++ client/src/components/ManageInventories.vue
@@ -5,6 +5,7 @@
     ></inventories>
     <save-inventory-form
       :inventory="inventoryInForm"
+      v-on:submit="onFormSave"
     ></save-inventory-form>
   </section>
 </template>
@@ -23,11 +24,13 @@ export default {
     return {
       inventories: null,
       inventoryInForm: {
-        id: null,
+        // id: null,
         name: '',
         description: '',
         condition: '',
         price: '',
+        userId: 1,
+        productId: 3,
       }
     }
   },
@@ -38,11 +41,15 @@ export default {
     // talk to back end server to retrieve all inventories
     retrieveInventory: async () => {
       const response = await InventoryService.retrieveInventory();
-      console.log('response resources:', response.data.resources);
+      // console.log('response resources:', response.data.resources);
       return response.data.resources;
 
       // console.log(this.inventories);
     },
+    onFormSave: async (inventory) => {
+      const response = await InventoryService.createInventory(inventory);
+      console.log(response.data);
+   },
     onEditClicked (inventories) {
       this.inventoryInForm = { ...inventories }
     },

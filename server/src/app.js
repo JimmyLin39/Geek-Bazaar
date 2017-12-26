f83a28a040b782966c9c90822a9ab69134725dc0
Kang Wang <kwang.inbox@gmail.com>
2017-12-26 16:25:08 -0500

    Fixed most Linting errors...

--- server/src/app.js
+++ server/src/app.js
@@ -11,31 +11,30 @@ const knex = require('knex')(knexConfig[ENV]);
 const knexLogger = require('knex-logger');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const sessdion = require('cookie-session');
+const session = require('cookie-session');
 
 const app = express();
-
-// Seperated Routes for each Resource
 const inventoriesRoutes = require('./routes/inventories');
-const usersRoutes = require('./routes/users');
+// const usersRoutes = require('./routes/users');
 
 app.use(cors());
-app.use(morgan('dev')); // see docs
-
+app.use(morgan('dev'));
+app.use(session({
+  name: 'session',
+  keys: ['secret'],
+}));
 app.use(knexLogger(knex));
 app.use(bodyParser.json());
-
 app.use('/inventories', inventoriesRoutes(knex));
 
 app.post('/login', (req, res) => {
-
   if (
     !req.body.email ||
     !req.body.password
   ) {
     res.send({
-      message: 'All fields must be filled in!'
-    })
+      message: 'All fields must be filled in!',
+    });
   } else {
     knex('users')
       .where({
@@ -44,31 +43,34 @@ app.post('/login', (req, res) => {
       .then((results) => {
         if (!results.length) {
           res.send({
-            message: 'Non-registered email! Please register, first!'
-          })
+            message: 'Non-registered email! Please register, first!',
+          });
         } else {
           knex('users')
             .where({
-              email: req.body.email
+              email: req.body.email,
             })
             .then((results) => {
               if (!bcrypt.compareSync(req.body.password, results[0].password)) {
-                return Promise.reject({
-                  message: 'The password is incorrect!'
-                })
+                Promise.reject({
+                  message: 'The password is incorrect!',
+                });
+              } else {
+                res.send({
+                  message: 'Correct Password!',
+                });
               }
             }).catch((error) => {
               res.send({
-                message: error.message
-              })
-            })
+                message: error.message,
+              });
+            });
         }
-      })
+      });
   }
-})
+});
 
 app.post('/register', (req, res) => {
-
   if (
     !req.body.full_name ||
     !req.body.display_name ||
@@ -76,8 +78,8 @@ app.post('/register', (req, res) => {
     !req.body.password
   ) {
     res.send({
-      message: 'All the fields must be filled!'
-    })
+      message: 'All the fields must be filled!',
+    });
   } else {
     knex('users')
       .where({
@@ -85,16 +87,16 @@ app.post('/register', (req, res) => {
       })
       .then((results) => {
         if (results.length) {
-          return res.send({
-            message: 'Email is already registered! Register with another one!'
-          })
+          res.send({
+            message: 'Email is already registered! Register with another one!',
+          });
         } else {
           const newUser = {
             full_name: req.body.full_name,
             display_name: req.body.display_name,
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password, 10),
-          }
+          };
           knex.insert(newUser)
             .into('users')
             .then(res.send({
@@ -102,16 +104,16 @@ app.post('/register', (req, res) => {
               You've successfully registered with the display name
               ${req.body.display_name}, and email ${req.body.email}.`,
             }))
-            .catch(err => {
+            .catch((err) => {
               console.log(err.message);
-            })
-          }
-        })
-      }
-})
+            });
+        }
+      });
+  }
+});
 
 // require('./routes')(app)
 
 app.listen(PORT, () => {
-  console.log('Geek-Bazzar Api listening on port ' + PORT);
+  console.log(`Geek-Bazzar Api listening on port ${PORT}`);
 });

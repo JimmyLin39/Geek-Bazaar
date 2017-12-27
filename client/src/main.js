9eb3fb18f988eca0e472466c3d890868dba6fc3b
JimmyLin39 <jimmylin0309@email.com>
2017-12-26 23:01:07 -0800

    add store from vuex

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-// import global styles
-import './styles/style.scss';
-
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App';
 import router from './router';
+import store from './vuex/store';
 
+// import global styles
+import './styles/style.scss';
+
+// set the API root so we can use relative url's in our actions.
+// Vue.http.options.root = 'http://localhost:8081';
 
 Vue.config.productionTip = false;
 
@@ -20,4 +23,5 @@ new Vue({
   router,
   template: '<App/>',
   components: { App },
+  store,
 });

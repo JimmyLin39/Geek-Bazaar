a1558843eb49c98bd72b86c1aee495b5543902ac
Kang Wang <kwang.inbox@gmail.com>
2017-12-27 15:47:33 -0500

    Updated

--- server/src/knexfile.js
+++ server/src/knexfile.js
@@ -6,20 +6,20 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL,
     },
     migrations: {
       directory: './db/migrations',
-      tableName: 'migrations'
+      tableName: 'migrations',
     },
     seeds: {
-      directory: './db/seeds'
-    }
+      directory: './db/seeds',
+    },
   },
 
   production: {
@@ -27,11 +27,11 @@ module.exports = {
     connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'migrations'
-    }
-  }
+      tableName: 'migrations',
+    },
+  },
 
 };

06b08c3ad83626725688ac75b9a59a43e7fe8180
JimmyLin39 <jimmylin0309@email.com>
2017-12-27 10:30:19 -0800

    add saveInventory and createInventory

--- client/src/vuex/modules/inventories/actions.js
+++ client/src/vuex/modules/inventories/actions.js
@@ -3,7 +3,7 @@ import InventoryService from '@/services/InventoryService';
 
 import {
   // FETCH_INVENTORY,
-  FETCH_INVENTORIES,
+  FETCH_INVENTORIES, CREATE_INVENTORY,
   // CREATE_INVENTORY,
   // UPDATE_INVENTORY,
   // DELETE_INVENTORY,
@@ -19,15 +19,16 @@ export function fetchInventories({ commit }) {
     .then((response) => commit(FETCH_INVENTORIES, response.data.resources))
 }
 
-// export function createProduct ({ commit }, { product, image }) {
-//   return Vue.http.post('products', product)
-//     .then((response) => {
-//       commit(CREATE_PRODUCT, response.body.data)
-//       return response.body.data.id
-//     })
-//     // Only upload image if an image has been defined
-//     .then((productId) => image && uploadProductImage({ commit }, image, productId))
-// }
+export function createInventory({ commit }, { inventory, image }) {
+  return InventoryService.createInventory(inventory)
+    .then((response) => {
+      console.log(response.data.resources);
+      commit(CREATE_INVENTORY, response.data.resources);
+      return response.data.resources.id;
+    });
+    // Only upload image if an image has been defined
+    // .then((productId) => image && uploadProductImage({ commit }, image, productId))
+}
 
 // export function updateProduct ({ commit }, { product, image }) {
 //   return Vue.http.put(`products/${product.id}`, product)
@@ -41,16 +42,16 @@ export function fetchInventories({ commit }) {
 //     .then((response) => commit(DELETE_PRODUCT, productId))
 // }
 
-// export function saveProduct ({ commit, state }, { product, image }) {
-//   const index = state.all.findIndex((p) => p.id === product.id)
+export function saveInventory ({ commit, state }, { inventory, image }) {
+  const index = state.all.findIndex(p => p.id === inventory.id);
 
-//   // update product if it exists or create it if it doesn't
-//   if (index !== -1) {
-//     return updateProduct({ commit }, { product, image })
-//   } else {
-//     return createProduct({ commit }, { product, image })
-//   }
-// }
+  // update product if it exists or create it if it doesn't
+  if (index !== -1) {
+    return updateInventory({ commit }, { inventory, image });
+  } else {
+    return createInventory({ commit }, { inventory, image });
+  }
+}
 
 // function uploadProductImage ({ commit }, image, productId) {
 //   var formData = new global.FormData()

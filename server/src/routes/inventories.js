a6120df3f8a915c570e44cba0b4e2a035108c164
JimmyLin39 <jimmylin0309@email.com>
2017-12-27 13:17:09 -0800

    add PUT /inventories/:id

--- server/src/routes/inventories.js
+++ server/src/routes/inventories.js
@@ -52,6 +52,38 @@ module.exports = (knex) => {
       });
   });
 
+  router.put('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const { userId, productId, name, description, condition, price } = req.body;
+    knex('inventories')
+      .where('inventories.id', id)
+      .update({
+        user_id: userId,
+        product_id: productId,
+        name,
+        description,
+        condition,
+        price,
+      })
+      .then(() => {
+        knex('inventories')
+          .join('products', 'inventories.product_id', '=', 'products.id')
+          .where('inventories.id', id)
+          .select('inventories.id', 'user_id', 'inventories.name', 'inventories.description', 'inventories.price', 'condition', 'image', 'thumbnail')
+          .then((resources) => {
+            res.send({
+              resources,
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
   return router;
 };
 

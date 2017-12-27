29fd8d7582844acbd9eb46ddb42e5b67f3845afe
Kang Wang <kwang.inbox@gmail.com>
2017-12-27 16:12:26 -0500

    small changes

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,4 +3,4 @@ const AuthenticationController = require('./controllers/AuthenticationController
 module.exports = (app) => {
   app.post('/register', AuthenticationController.register);
   app.post('/login', AuthenticationController.login);
-}
+};

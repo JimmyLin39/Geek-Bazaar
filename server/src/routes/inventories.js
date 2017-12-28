54b60676e13ea9b58bf163d9bcf5c8085bb50fd8
JimmyLin39 <jimmylin0309@email.com>
2017-12-27 18:42:48 -0800

    and PUT /upload , GET /:id and comments

--- server/src/routes/inventories.js
+++ server/src/routes/inventories.js
@@ -1,8 +1,46 @@
 const express = require('express');
-
+const sharp = require('sharp');
+const multer = require('multer');
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 module.exports = (knex) => {
+
+  // dest: `${__dirname}/../public/uploads`
+  // Upload product image and assign it to product, declare before product update
+  // so it gets dibs on the route matching
+  router.put('/upload', upload.single('inventory_image'), (req, res) => {
+    const inventoryId = req.body.inventory_id;
+    const filename = inventoryId + Date.now();
+
+    sharp(req.file.buffer)
+      .resize(200, 200)
+      .crop(sharp.strategy.entropy)
+      .toFile(`${__dirname}/../../public/uploads/${filename}`, (err) => {
+        if (err) {
+          console.error('woops', err);
+        }
+      });
+    knex('inventories')
+      .where('inventories.id', inventoryId)
+      .update({
+        image_url: `http://localhost:8081/uploads/${filename}`,
+        image_name: req.file.originalname
+      })
+      .then(() => {
+        res.status(201);
+        res.send({
+          message: req.file.originalname,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+
   // retrieve all inventories
   router.get('/', (req, res) => {
     // if (!req.session.user_id) {
@@ -20,6 +58,26 @@ module.exports = (knex) => {
       });
   });
 
+  // retrieve one inventory
+  router.get('/:id', (req, res) => {
+    // if (!req.session.user_id) {
+    //   res.redirect('/users');
+    // }
+    const id = Number(req.params.id);
+    knex('inventories')
+      .select()
+      .where('inventories.id', id)
+      .then((resources) => {
+        res.send({
+          resources,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  // Create a new inventory 
   router.post('/', (req, res) => {
     const { userId, name, description, condition, price } = req.body;
     knex('inventories')
@@ -30,6 +88,7 @@ module.exports = (knex) => {
         condition,
         price,
       })
+      // send back inserted inventory
       .then(() => {
         knex('inventories')
           .select()
@@ -49,6 +108,7 @@ module.exports = (knex) => {
       });
   });
 
+  // Update an inventory
   router.put('/:id', (req, res) => {
     const id = Number(req.params.id);
     const { userId, name, description, condition, price } = req.body;
@@ -61,6 +121,7 @@ module.exports = (knex) => {
         condition,
         price,
       })
+      // send back updated inventory
       .then(() => {
         knex('inventories')
           .where('inventories.id', id)
@@ -79,6 +140,7 @@ module.exports = (knex) => {
       });
   });
 
+  // delete an inventory
   router.delete('/:id', (req, res) => {
     const id = Number(req.params.id);
     knex('inventories')

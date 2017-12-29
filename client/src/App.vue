756384583d84ad35be3eda7593a20d87c2b07318
JimmyLin39 <jimmylin0309@email.com>
2017-12-29 15:46:43 -0800

    remove head info

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
     <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <link rel="icon" href="../../favicon.ico">
-
       <title>Geek Bazaar</title>
-
-      <!-- Bootstrap core CSS -->
-      <link href="../bootstrap-4.0.0-alpha.6/dist/css/bootstrap.min.css" rel="stylesheet">
-
-      <!-- Custom styles for this template -->
-      <link href="../bootstrap-4.0.0-alpha.6/docs/examples/jumbotron/jumbotron.css" rel="stylesheet">
     </head>
-    <header class=“app-header”>
-      <Navbar></Navbar>
+    <header class="app-header">
+      <navbar></navbar>
       <!-- <Pagefooter></Pagefooter> -->
     </header>
-    <main class=“container”>
+    <main class="container">
       <router-view></router-view>
     </main>
-
   </div>
 </template>
 

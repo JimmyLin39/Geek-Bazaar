6d8408a3fe56663af27d47d7dcfe368fed8cde6d
JimmyLin39 <jimmylin0309@email.com>
2017-12-31 14:46:46 -0800

    make functions talk to backend

--- client/src/vuex/modules/cart/actions.js
+++ client/src/vuex/modules/cart/actions.js
@@ -1,19 +1,41 @@
+import CartService from "@/services/CartService";
+
 import {
+  FETCH_CART,
   ADD_TO_CART,
   REMOVE_FROM_CART,
   SUBTRACT_FROM_CART
 } from './mutation-types'
 
-export function addToCart ({ commit, state }, inventory) {
-  const record = state.inventories.find((p) => p.id === inventory.id)
+export function fetchCart({ commit }) {
+  return CartService.retrieveCart()
+    .then(response => {
+      console.log('cart', response.data.resources);
+      
+      commit(FETCH_CART, response.data.resources)
+    });
+}
 
-  if (!record || record.quantity < 1) {
-    commit(ADD_TO_CART, inventory)
-  }
+export function addToCart ({ commit, state }, inventory) {
+  // FIXME: update to current userId
+  const userId = 1;
+  return CartService.addToCart(inventory, userId)
+    .then((response) => {
+      console.log(response.data.resources);
+        
+      const record = state.inventories.find((p) => p.id === inventory.id)
+      
+      if (!record || record.quantity < 1) {
+        commit(ADD_TO_CART, inventory)
+      }
+    })
 }
 
 export function removeFromCart ({ commit }, inventoryId) {
-  commit(REMOVE_FROM_CART, inventoryId)
+  return CartService.removeFromCart(inventoryId)
+    .then(() => {
+      commit(REMOVE_FROM_CART, inventoryId);
+    })
 }
 
 export function subtractFromCart ({ commit }, inventoryId) {

47a26ea777b95d10de2ab3c51cd5d575eb3ed27b
JimmyLin39 <jimmylin0309@email.com>
2018-01-02 11:30:32 -0800

    fix lint

--- client/src/vuex/modules/cart/actions.js
+++ client/src/vuex/modules/cart/actions.js
@@ -1,51 +1,48 @@
-import CartService from "@/services/CartService";
+import CartService from '@/services/CartService';
 
 import {
   FETCH_CART,
   ADD_TO_CART,
   REMOVE_FROM_CART,
   SUBTRACT_FROM_CART,
-  RESET_CART
-} from './mutation-types'
+  RESET_CART,
+} from './mutation-types';
 
 export function fetchCart({ commit }) {
   return CartService.retrieveCart()
-    .then(response => {
+    .then((response) => {
       console.log('cart', response.data.resources);
-      
-      commit(FETCH_CART, response.data.resources)
+      commit(FETCH_CART, response.data.resources);
     });
 }
 
-export function addToCart ({ commit, state }, inventory) {
+export function addToCart({ commit, state }, inventory) {
   // FIXME: update to current userId
   const userId = 1;
   return CartService.addToCart(inventory, userId)
     .then((response) => {
       console.log(response.data.resources);
-        
-      const record = state.inventories.find((p) => p.id === inventory.id)
-      
+      const record = state.inventories.find(p => p.id === inventory.id);
       if (!record || record.quantity < 1) {
-        commit(ADD_TO_CART, inventory)
+        commit(ADD_TO_CART, inventory);
       }
-    })
+    });
 }
 
-export function removeFromCart ({ commit }, inventoryId) {
+export function removeFromCart({ commit }, inventoryId) {
   return CartService.removeFromCart(inventoryId)
     .then(() => {
       commit(REMOVE_FROM_CART, inventoryId);
-    })
+    });
 }
 
-export function subtractFromCart ({ commit }, inventoryId) {
-  commit(SUBTRACT_FROM_CART, inventoryId)
+export function subtractFromCart({ commit }, inventoryId) {
+  commit(SUBTRACT_FROM_CART, inventoryId);
 }
 
-export function checkoutCart ({ commit, state }) {
+export function checkoutCart({ commit, state }) {
   // calculate total price for all cart items
-  let orders = [];
+  const orders = [];
   state.inventories.forEach((element) => {
     orders.push({
       // FIXME: update to current userID
@@ -54,14 +51,13 @@ export function checkoutCart ({ commit, state }) {
       total_cents: element.price,
       status: 'waiting',
       type: 'sale',
-    })
-  })
+    });
+  });
   // add orders to orders table
   return CartService.checkoutCart(orders)
     .then((response) => {
-      console.log('return data:', response.data.resources);
+      console.log('response:', response.data.message);
       // reset cart
-      commit(RESET_CART)
-    })
-  
+      commit(RESET_CART);
+    });
 }

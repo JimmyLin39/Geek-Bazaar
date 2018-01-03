6ea07604c5bee77318ceb12979a803edf5020560
JimmyLin39 <jimmylin0309@email.com>
2018-01-02 16:57:01 -0800

    select order created_date

--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -11,9 +11,10 @@ module.exports = (knex) => {
     knex('orders')
       .join('inventories', 'inventory_id', 'inventories.id')
       .join('users', 'orders.seller_id', 'users.id')
-      .select('orders.id', 'inventories.name', { seller: 'users.full_name' }, 'total_cents', 'status', 'date_of_agreement', 'image_url', 'image_name')
+      .select('orders.id', 'orders.created_at', 'inventories.name', { seller: 'users.full_name' }, 'total_cents', 'status', 'date_of_agreement', 'image_url', 'image_name')
     // FIXME: update to the current userID
       .where('buyer_id', 1)
+      .orderBy('orders.id', 'desc')
       .then((resources) => {
         res.send({
           resources,

040086ce24a454f94f28060e5d50fa616e37079e
JimmyLin39 <jimmylin0309@email.com>
2018-01-03 11:46:33 -0800

    fix lint

--- client/src/vuex/modules/cart/index.js
+++ client/src/vuex/modules/cart/index.js
@@ -1,17 +1,17 @@
-import * as getters from './getters'
-import * as actions from './actions'
+import * as getters from './getters';
+import * as actions from './actions';
 
 import {
   FETCH_CART,
   ADD_TO_CART,
   REMOVE_FROM_CART,
-  SUBTRACT_FROM_CART
-} from './mutation-types'
+  SUBTRACT_FROM_CART,
+} from './mutation-types';
 import { RESET_CART } from './mutation-types';
 
 const initialState = {
-  inventories: []
-}
+  inventories: [],
+};
 
 // mutations
 const mutations = {
@@ -19,7 +19,7 @@ const mutations = {
     // assign the inventories that we got from our FETCH_CART event to state.inventories
     state.inventories = inventories;
   },
-  [ADD_TO_CART] (state, inventory) {
+  [ADD_TO_CART](state, inventory) {
     const record = state.inventories.find(p => p.id === inventory.id)
     if (!record) {
       state.inventories.push({
@@ -27,31 +27,31 @@ const mutations = {
         name: inventory.name,
         price: inventory.price,
         user_id: inventory.user_id,
-        quantity: 1
-      })
+        quantity: 1,
+      });
     } else {
-      record.quantity++
+      record.quantity++;
     }
   },
-  [REMOVE_FROM_CART] (state, inventoryId) {
+  [REMOVE_FROM_CART](state, inventoryId) {
     state.inventories = state.inventories.filter(p => p.id !== inventoryId)
   },
-  [SUBTRACT_FROM_CART] (state, inventoryId) {
+  [SUBTRACT_FROM_CART](state, inventoryId) {
     const record = state.inventories.find(p => p.id === inventoryId)
     if (record && record.quantity === 1) {
       state.inventories = state.inventories.filter(p => p.id !== inventoryId)
     } else if (record) {
-      record.quantity--
+      record.quantity--;
     }
   },
-  [RESET_CART] (state) {
-    state.inventories = initialState.inventories
-  }
-}
+  [RESET_CART](state) {
+    state.inventories = initialState.inventories;
+  },
+};
 
 export default {
-  state: {...initialState},
+  state: { ...initialState },
   getters,
   actions,
-  mutations
-}
+  mutations,
+};

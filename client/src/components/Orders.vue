4f9597e7a897c9c611882d13099003fc9d94e3de
JimmyLin39 <jimmylin0309@email.com>
2018-01-02 16:04:51 -0800

    add order info

--- client/src/components/Orders.vue
+++ client/src/components/Orders.vue
@@ -4,17 +4,26 @@
     <table class="table table-responsive table-hover inventory-table">
       <thead>
         <tr>
-          <th class="inventory-name-col">Name</th>
-          <th class="inventory-price-col">Price</th>
-          <th class="inventory-delete-col"></th>
+          <th></th>
+          <th>Order Id</th>
+          <th>Name</th>
+          <th>Seller</th>
+          <th>Price</th>
+          <th>Status</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="order in orders" v-if="order.buyer_id === 1 " track-by="id">
-          <td>{{inventory.name}}</td>
-          <td>${{order.price}}</td>
-          <td><a href="#">remove</a></td>
-          <!-- v-on:click.prevent.stop="onRemove(inventory.id) -->
+        <tr v-for="order in orders" track-by="id">
+          <td>
+            <img v-bind:src="order.image_url" v-bind:alt="order.image_name" class="inventory-image">
+          </td>
+          <td>{{order.id}}</td>
+          <td>{{order.name}}</td>
+          <td>{{order.seller}}</td>
+          <td>${{order.total_cents}}</td>
+          <td>{{order.status}}</td>
+          <td><a href="#" v-on:click.prevent.stop="onRemove(inventory.id)">Cancel</a></td>
         </tr>
         <tr v-if="!orders">
           <td colspan="5" class="p-y-3 text-xs-center">
@@ -30,18 +39,26 @@
 import OrderService from '@/services/OrderService';
 
 export default {
-  data () {
+  data() {
     return {
       orders: []
     }
   },
-  computed() {
-    return OrderService.retrieveOrders()
-      .then((response) => {
-        console.log('orders', response.data.resources);
-        
-      });
+  created: function() {
+    this.retrieveOrders(this.orders);
+    console.log(this.orders);
+    
+  },
+  methods: {
+    retrieveOrders: async (orders) => {
+      const response = await OrderService.retrieveOrders();
+      // console.log('orders', response.data.resources);
+      response.data.resources.forEach((element) => {
+        orders.push(element);
+      })
+    } 
   }
+  
 
 }
 </script>

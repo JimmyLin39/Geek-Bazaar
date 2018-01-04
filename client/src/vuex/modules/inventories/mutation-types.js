143f71d74559a6cec7eeb2bbb138ab75210b6044
JimmyLin39 <jimmylin0309@email.com>
2018-01-03 16:29:41 -0800

    add FETCH_SEARCH

--- client/src/vuex/modules/inventories/mutation-types.js
+++ client/src/vuex/modules/inventories/mutation-types.js
@@ -3,3 +3,4 @@ export const FETCH_INVENTORIES = 'inventories/FETCH_INVENTORIES';
 export const CREATE_INVENTORY = 'inventories/CREATE_INVENTORY';
 export const UPDATE_INVENTORY = 'inventories/UPDATE_INVENTORY';
 export const DELETE_INVENTORY = 'inventories/DELETE_INVENTORY';
+export const FETCH_SEARCH = 'inventories/FETCH_SEARCH';

37bdc97dcde06c334d86e4ad97593393fb120b5f
JimmyLin39 <jimmylin0309@email.com>
2018-01-05 13:12:09 -0800

    add userId from response.data

--- client/src/vuex/modules/login/actions.js
+++ client/src/vuex/modules/login/actions.js
@@ -5,16 +5,14 @@ import {
 } from './mutation-types';
 
 export function login({ commit, state }, credentials) {
-  console.log("We're in the actions!");
-  console.log('credentials.email', credentials.email);
-  console.log('credentials.password', credentials.password);
+  // check user credentials
   return AuthenticationService.login({
     email: credentials.email,
     password: credentials.password,
   }).then((response) => {
-    console.log((response.data.cookies).toString());
     const cookies = response.data.cookies;
-    commit(SETUP_COOKIES, cookies);
+    const userId = response.data.userId;
+    commit(SETUP_COOKIES, { cookies, userId });
   });
 }
 

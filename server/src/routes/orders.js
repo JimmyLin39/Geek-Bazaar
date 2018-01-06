536cde0d04c96f9218a110acbc76e53640c814ff
JimmyLin39 <jimmylin0309@email.com>
2018-01-05 17:54:58 -0800

    update to the current userID

--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 module.exports = (knex) => {
   // retrieve all orders
-  router.get('/', (req, res) => {
+  router.get('/:id', (req, res) => {
     // if (!req.session.user_id) {
     //   res.redirect('/users');
     // }
@@ -12,8 +12,7 @@ module.exports = (knex) => {
       .join('inventories', 'inventory_id', 'inventories.id')
       .join('users', 'orders.seller_id', 'users.id')
       .select('orders.id', 'orders.created_at', 'inventories.name', { seller: 'users.full_name' }, 'total_cents', 'status', 'date_of_agreement', 'image_url', 'image_name')
-    // FIXME: update to the current userID
-      .where('buyer_id', 1)
+      .where('buyer_id', req.params.id)
       .orderBy('orders.id', 'desc')
       .then((resources) => {
         res.send({

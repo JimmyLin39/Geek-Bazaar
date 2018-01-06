affafbca94862c3dd454851a8985b8f4d378292b
Kang Wang <kwang.inbox@gmail.com>
2018-01-05 18:14:00 -0800

    Pulled user id info from knex query into the Vue front end

--- client/src/components/Users.vue
+++ client/src/components/Users.vue
@@ -1,23 +1,45 @@
 <template>
-
+<div class="container">
+  <div class='user-info'>
+    <h1>User Information</h1>
+    <div class='user-info-details'>
+      <span><p>Full Name: {{ user[0].full_name }} </p></span>
+      <span><p>Dispaly Name: {{ user[0].display_name }} </p></span>
+      <span><p>Email: {{ user[0].email }}</p></span>
+      <span><p>Password: {{ user[0].password }}</p></span>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import usersService from '@/services/usersService';
 
 export default {
   data() {
     return {
-
-    };
+      user: [],
+      userId: this.$cookie.get('userId'),
+    }
+  },
+  created: function() {
+    this.retrieveUser(this.user, this.userId);
   },
   methods: {
-
-  };
+    retrieveUser: async (user, userId) => {
+      const response = await usersService.retrieveUser(userId);
+      console.log(response.data.user[0]);
+      // console.log('\nresponse: ', response.data.userInfo);
+      user.push(response.data.user[0]);
+    }
+  }
 }
-
 </script>
 
-<style>
 
+<style lang="scss" scoped>
+.user-info {
+  padding-top: 20px;
+}
 
 </style>

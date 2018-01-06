b69ca15400fae3e92b9a2b69df3a3d563dc30d20
JimmyLin39 <jimmylin0309@email.com>
2018-01-06 14:03:24 -0800

    initial Messages.vue

--- client/src/components/Messages.vue
+++ client/src/components/Messages.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Message</h1>
+  <div id="messages">
+    <h1>Message</h1>
+    {{ messages[0] }}
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  created () {
+    this.$store.dispatch('fetchMessages');
+  },
+  computed: {
+    ...mapGetters({
+      messages: 'getMessages'
+    })
+  },
+  methods: {
+  },
+}
+</script>

18e903235f419de1a5c5b208bb963005b2fdc1b9
JimmyLin39 <jimmylin0309@email.com>
2018-01-05 16:02:08 -0800

    add 'please login'

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -4,7 +4,7 @@
       <div id="loginbox" style="margin-top:50px;"class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info" >
           <div class="panel-heading">
-            <div class="panel-title">Log In</div>
+            <div class="panel-title">Please Log In</div>
           </div>
           <div style="padding-top:30px" class="panel-body"></div>
             <form id="loginform" class="form-horizontal" role="form">
